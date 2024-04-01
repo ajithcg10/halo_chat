65fd15dd8391fe5b2ffe84dcde935424a49df547
ajith.woodenclouds <ajith.woodenclouds@gmail.com>
2024-04-02 01:35:31 +0530

    just-cheking

--- app/(root)/chats/page.tsx
+++ app/(root)/chats/page.tsx
@@ -4,6 +4,7 @@ import Contacts from "../contacts/page";
 
 export default function Chtas() {
   // Render component
+  "ajith";
   return (
     <div className="main-container wrapper">
       <div className="w-1/3 max-lg:w-1/2 max-md:w-full">
